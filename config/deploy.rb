1945dfda429d7b9e336fe3a92ba8de7bb77fcb85
Doug Nielsen <doug@nielsenfamily.ca>
2012-03-17 18:24:28 -0400

    Syntax

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,9 +11,11 @@ set :repository,  "git@github.com:Gajol/SwimMeetUpload.git"
 set :scm, 'git'  # syntax from http://help.github.com/deploy-with-capistrano/
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, domain                          # Your HTTP server, Apache/etc
-role :app, domain                          # This may be the same as your `Web` server
-role :db,  domain, :primary => true 		# This is where Rails migrations will run
+
+# roles - https://github.com/capistrano/capistrano/wiki/Roles
+role :web, 'www.nielsenfamily.ca'                          # Your HTTP server, Apache/etc
+role :app, 'www.nielsenfamily.ca'                          # This may be the same as your `Web` server
+role :db,  'mysql.nielsenfamily.ca', :primary => true 		# This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -23,6 +25,8 @@ role :db,  domain, :primary => true 		# This is where Rails migrations will run
 set :deploy_to, "nielsenfamily.ca"
 set :deploy_via, :remote_cache   # dreamhost says this
 # set :deploy_via, :export  # syntax from http://help.github.com/deploy-with-capistrano
+# http://guides.beanstalkapp.com/deployments/deploy-with-capistrano.html
+# cache speeds up when cache is used http://guides.beanstalkapp.com/deployments/deploy-with-capistrano.html
 set :branch, 'master'
 set :git_shallow_clone, 1
 set :scm_verbose, true
