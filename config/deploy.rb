859491a0b8e4b69dc70a4018e11b720e30aaf5cc
Doug Nielsen <doug@nielsenfamily.ca>
2012-03-17 17:59:59 -0400

    Commit first draft of deploy.rb and database.yml production file (Dreamhost)

---
+++ config/deploy.rb
@@ -0,0 +1,3 @@
+#trying to follow Dreamhost instructions
+set :scm_command, "/usr/bin/git" #updated version of git on ?server in user directory
+set :local_scm_command, "git" #correct path to local ?git 
