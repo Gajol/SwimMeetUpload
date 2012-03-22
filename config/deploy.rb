a252123c6ddad7ebd3bc2f1b199e736d56ccc5b0
Doug Nielsen <doug@nielsenfamily.ca>
2012-03-22 11:21:34 -0400

    Change to Valby PC Workshop

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,9 @@
 set :scm_command, "/usr/bin/git" #updated version of git on ?server in user directory
 set :local_scm_command, "git" #correct path to local ?git 
 
-set :application, "www.nielsenfamily.ca"
+# set :application, "www.nielsenfamily.ca"
+set :application, "upload"
+
 
 # version control config
 set :scm_username, 'Gajol'
@@ -12,23 +14,28 @@ set :scm, 'git'  # syntax from http://help.github.com/deploy-with-capistrano/
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 #dreamhost does not give sudo access - https://github.com/capistrano/capistrano/wiki/2.x-From-The-Beginning
-set :user, 'dounie1'   # since userid is different than home pc "hence the "DOUG" warhning"
+# # set :user, 'dounie1'   # since userid is different than home pc "hence the "DOUG" warhning"
+# # set :use_sudo, false   # https://github.com/capistrano/capistrano/wiki/2.x-From-The-Beginning 
+set :user, 'webmaster'   
 set :use_sudo, false   # https://github.com/capistrano/capistrano/wiki/2.x-From-The-Beginning 
-
 
 # roles - https://github.com/capistrano/capistrano/wiki/Roles
 # capistrano ssh's to these addresses
-role :web, 'nielsenfamily.ca'                          # Your HTTP server, Apache/etc
-role :app, 'nielsenfamily.ca'                          # This may be the same as your `Web` server
-# role :db,  'mysql.nielsenfamily.ca', :primary => true 		# This is where Rails migrations will run
-role :db,  'nielsenfamily.ca', :primary => true 		# This is where Rails migrations will run
+role :web, '192.168.1.121'                          # Your HTTP server, Apache/etc
+role :app, '192.168.1.121'                          # This may be the same as your `Web` server
+role :db,  '192.168.1.121', :primary => true 		# This is where Rails migrations will run
+
+# # role :web, 'nielsenfamily.ca'                          # Your HTTP server, Apache/etc
+# # role :app, 'nielsenfamily.ca'                          # This may be the same as your `Web` server
+# # # role :db,  'mysql.nielsenfamily.ca', :primary => true 		# This is where Rails migrations will run
+# # role :db,  'nielsenfamily.ca', :primary => true 		# This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # deploy config
 # git-specific based on dreamhost ( http://wiki.dreamhost.com/Capistrano )
-set :deploy_to, "~/nielsenfamily.ca"
+set :deploy_to, "/webapps/upload"
 set :deploy_via, :remote_cache   # dreamhost says this
 # set :deploy_via, :export  # syntax from http://help.github.com/deploy-with-capistrano
 # http://guides.beanstalkapp.com/deployments/deploy-with-capistrano.html
